21a7124f7232041580955f1328c72137777768f0
Mohammad El Ejel <melejel@seglb.com>
2025-01-21 16:29:39 +0200

    STOCK CONTROLLER CHANGES

--- Models/InStock.cs
+++ Models/InStock.cs
@@ -1,34 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PAMAPIs.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PAMAPIs.Models;
-
-public partial class InStock
+namespace PAM.Models.EntityModels
 {
-    public int InId { get; set; }
+    [Table("InStock")]
+    public class InStock
+    {
+        [Key]
+        public int InId { get; set; }
+        public int InNo { get; set; }
+        public string RefNo { get; set; }
+        public double Quantity { get; set; }
+        public DateTime Date { get; set; }
+        public int ItemId { get; set; }
+        public int SiteId { get; set; }
+        public int POId { get; set; }
+        public int PODetailId { get; set; }
+        public int UsrId { get; set; }
+        public string SuppDeliveryNote { get; set; }
+        public int? FromSiteId { get; set; }
+        public string Remarks { get; set; }
+        public int? OutId { get; set; }
 
-    public int? InNo { get; set; }
+        // Navigation properties
+        [ForeignKey("SiteId")]
+        public virtual Site GetSites { get; set; }
 
-    public string? RefNo { get; set; }
+        [ForeignKey("ItemId")]
+        public virtual Item GetItems { get; set; }
 
-    public double? Quantity { get; set; }
+        [ForeignKey("POId")]
+        public virtual PurchaseOrder GetPurchase_Order { get; set; }
 
-    public DateTime? Date { get; set; }
+        [ForeignKey("PODetailId")]
+        public virtual PoDetail GetPO_Detail { get; set; }
 
-    public int? ItemId { get; set; }
+        [ForeignKey("UsrId")]
+        public virtual User GetUsers { get; set; }
 
-    public int SiteId { get; set; }
-
-    public int? Poid { get; set; }
-
-    public int? PodetailId { get; set; }
-
-    public int? UsrId { get; set; }
-
-    public string? SuppDeliveryNote { get; set; }
-
-    public int POId { get; set; }
-
-    public int? PODetailId { get; set; }
-
+        [ForeignKey("FromSiteId")]
+        public virtual Site FromSite { get; set; }
+    }
 }
